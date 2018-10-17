f466eeab7e410bc85f867122be61b01a68b00aa7
MailboxValidator <support@mailboxvalidator.com>
2018-10-17 14:50:21 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setuptools.setup(
 	description="Email verification module for Flask using MailboxValidator API. It validates if the email is valid, from a free provider, contains high-risk keywords, whether it\'s a catch-all address and so much more.",
 	long_description=long_description,
 	long_description_content_type="text/markdown",
-	url="https://github.com/MailboxValidator/mailboxvalidator-python",
+	url="https://github.com/MailboxValidator/Flask_MailboxValidator",
 	packages=setuptools.find_packages(),
 	classifiers=(
 		"Development Status :: 5 - Production/Stable",
@@ -26,7 +26,6 @@ setuptools.setup(
 	install_requires=[
 		'Flask>=1.0.2',
 		'Flask-WTF>=0.14.2',
-		# 'nose2>=0.8.0',
 		'WTForms>=2.2.1',
 	],
 	extras_require={
